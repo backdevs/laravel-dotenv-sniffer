85119c7dc2677d9e1208ca13684507a5ab9ed0ac
Bogdan <bogdan@backdevs.net>
2023-03-09 14:38:00 +0200

    Refactor Directory and DirectoryValidator with more checks

--- src/Validator/Constraints/Directory.php
+++ src/Validator/Constraints/Directory.php
@@ -8,9 +8,13 @@ use Symfony\Component\Validator\Constraint;
 
 class Directory extends Constraint
 {
-    public string $notFoundMessage = 'The path "{{ path }}" could not be found.';
+    public const NOT_FOUND_ERROR = '5b4c6d8f-aecf-47cf-a268-63341330991b';
+    public const NOT_DIRECTORY_ERROR = '369d9a65-acfc-415c-a620-bf5e18468bd3';
+    public const NOT_READABLE_ERROR = 'a1b2d0ed-d6aa-40df-8be9-c98ebe5f8a32';
 
+    public string $notFoundMessage = 'The path "{{ path }}" could not be found.';
     public string $notDirectoryMessage = 'The path "{{ path }}" is not a directory.';
+    public string $notReadableMessage = 'The directory "{{ path }}" is not readable.';
 
     public string $mode = 'strict';
 }
--- src/Validator/Constraints/DirectoryValidator.php
+++ src/Validator/Constraints/DirectoryValidator.php
@@ -4,30 +4,50 @@ declare(strict_types=1);
 
 namespace Backdevs\DotenvSniffer\Validator\Constraints;
 
+use Stringable;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
 class DirectoryValidator extends ConstraintValidator
 {
     public function validate(mixed $value, Constraint $constraint): void
     {
         if (!$constraint instanceof Directory) {
+            throw new UnexpectedTypeException($constraint, Directory::class);
+        }
+
+        if ($value === null || $value === '') {
             return;
         }
 
-        $path = realpath($value);
+        if (!is_string($value) && !$value instanceof Stringable) {
+            throw new UnexpectedValueException($value, 'string');
+        }
 
-        if ($path === false) {
+        if (!file_exists($value)) {
             $this->context->buildViolation($constraint->notFoundMessage)
                 ->setParameter('{{ path }}', $value)
+                ->setCode(Directory::NOT_FOUND_ERROR)
                 ->addViolation();
 
             return;
         }
 
-        if (!is_dir($path)) {
+        if (!is_dir($value)) {
             $this->context->buildViolation($constraint->notDirectoryMessage)
                 ->setParameter('{{ path }}', $value)
+                ->setCode(Directory::NOT_DIRECTORY_ERROR)
+                ->addViolation();
+
+            return;
+        }
+
+        if (!is_readable($value)) {
+            $this->context->buildViolation($constraint->notReadableMessage)
+                ->setParameter('{{ path }}', $value)
+                ->setCode(Directory::NOT_READABLE_ERROR)
                 ->addViolation();
         }
     }
